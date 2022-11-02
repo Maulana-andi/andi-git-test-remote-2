6f30753c9a5f0015a7b258e88e7676de047e5f7d
Maulana-andi <andi_maulana9@ymail.com>
2022-11-02 10:14:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>mencoba git remote</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores inventore facilis nulla nam repellat alias ea quibusdam minima facere maxime! Maiores, hic eum magnam aut molestiae similique quae! Eveniet, explicabo?</p>
+    <ul>
+        <li>andi</li>
+        <li>maulana</li>
+    </ul>
 </body>
 </html>
